afb139f82636558178e87727579814d779b05652
Joe Woodhouse <joewoodhouse@outlook.com>
2015-04-07 15:26:16 +0100

    Add sinon and sinon-chai library. Add repository and bug urls to package.json

--- app/templates/_helpers.js
+++ app/templates/_helpers.js
@@ -1,4 +1,7 @@
 var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
 
 chai.should();
 chai.config.includeStack = true;
@@ -7,3 +10,4 @@ global.expect = chai.expect;
 global.AssertionError = chai.AssertionError;
 global.Assertion = chai.Assertion;
 global.assert = chai.assert;
+global.sinon = sinon;
