ba6fcb5f81784b69aa7c91268aa9b157e91411fd
Tom Spencer <fiznool@gmail.com>
2015-04-08 09:21:34 +0100

    Further tweaks to oauth generation

--- app/templates/_index.js
+++ app/templates/_index.js
@@ -32,37 +32,29 @@ var service = new sdk.Service({
      */
     strategy: null,
 
-    // These options will be passed to the strategy when registering
-    // For example, for OAuth 2.0 the options could be
-    //
-    //   options:{
-    //     clientID: process.env.MY_SERVICE_ID,
-    //     clientSecret: process.env.MY_SERVICE_SECRET
-    //   }
-    //
-    // ...and for OAuth 1.0 ...
-    //
-    //   options:{
-    //     consumerKey: process.env.TRELLO_KEY,
-    //     consumerSecret: process.env.TRELLO_SECRET,
-    //     trelloParams: {
-    //       scope: 'read,write',
-    //       name: 'Flow XO',
-    //       expiration: 'never'
-    //     }
-    //   }
+    // These options will be passed to the strategy when registering.
+    <% if(auth.type === 'oauth1') { %>// An OAuth 1.0 or 1.0a strategy requires `consumerKey` and `consumerSecret`
+    // to be passed. Fill in your key and secret for this service in the
+    // .env file and they will be populated at runtime below.
+    // If your strategy requires any other options to be passed when registering,
+    // add them below.
     options: {
-      <% if(auth.type === 'oauth1') { %>
-        consumerKey: process.env.<%= slug.toUpperCase()%>_KEY,
-        consumerSecret: process.env.<%= slug.toUpperCase()%>_SECRET
-      <% } else if (auth.type === 'oauth2') { %>
-        clientID: process.env.<%= slug.toUpperCase()%>_ID,
-        clientSecret: process.env.<%= slug.toUpperCase()%>_SECRET
-      <% } %>
+      consumerKey: process.env.<%= slug.toUpperCase()%>_KEY,
+      consumerSecret: process.env.<%= slug.toUpperCase()%>_SECRET
     },
-
+    <% } else if (auth.type === 'oauth2') { %>// An OAuth 2.0 strategy requires `clientID` and `clientSecret`
+    // to be passed. Fill in your ID and secret for this service in the
+    // .env file and they will be populated at runtime below.
+    // If your strategy requires any other options to be passed when registering,
+    // add them below.
+    options: {
+      clientID: process.env.<%= slug.toUpperCase()%>_ID,
+      clientSecret: process.env.<%= slug.toUpperCase()%>_SECRET
+    },
+    <% } %>
     // Authentication parameters to be used.
-    // For example where an OAuth 2.0 API defines access scopes,
+    // These are sent when making an OAuth request.
+    // For example, where an OAuth 2.0 API defines access scopes,
     // you may send
     // params:{
     //   scope: ['allow_email']
@@ -70,7 +62,6 @@ var service = new sdk.Service({
     params: {
 
     }
-
   }<% } %>
 });
 
